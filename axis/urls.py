87d528e304bba3e3e5c0d3d6cb54802c0f830319
Melody <leclecticat@gmail.com>
2020-05-01 14:40:38 -0700

    adds profile page which displays all user projects

--- axis/urls.py
+++ axis/urls.py
@@ -5,4 +5,10 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('geography/', views.geography, name='geography'),
     path('accounts/signup', views.signup, name='signup'),
+    #project management
+    path('accounts/profile', views.profile, name='profile'),
+    # path('profile/<int:project_id>/', views.project_details, name='project_details'),
+    # path('profile/<int:project_id>/delete', views.project_delete, name='project_delete'),
+    # path('profile/<int:project_id>/update', views.project_update, name='project_update'),
+    
 ]
--- axis/views.py
+++ axis/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
+from .models import Project
+from .forms import ProjectForm
 
-# Create your views here.
+
 from django.views.generic import ListView, DetailView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
@@ -28,3 +30,45 @@ def signup(request):
   context = {'form': form, 'error_message': error_message}
   return render(request, 'registration/signup.html', context)
 
+@login_required
+def profile(request):
+  error_message = ''
+  projects = Project.objects.filter(users=request.user)
+  
+  if request.method == 'POST':
+    project_form = ProjectForm(request.POST)
+    if project_form.is_valid() :
+      new_Project = project_form.save()
+      new_Project.users.add(request.user)
+      new_Project.save()
+      return redirect('profile')
+    else:
+      error_message = 'Invalid project - try again'
+    
+  else:
+    project_form = ProjectForm()
+  
+  return render(request, 'registration/profile.html', { 'projects': projects, 'project_form': project_form, 'error_message': error_message })
+
+# def project_details(request, project_id):
+# 	project = Project.objects.get(id=project_id)
+# 	project_form = ProjectForm()
+# 	return render(request, 'project_details.html', {'trip': trip, 'things': things, 'city': city })
+
+
+# def project_delete(request, trip_id):
+# 	Trip.objects.get(id=trip_id).delete()
+# 	return redirect('trips')
+
+
+# def project_update(request, trip_id):
+# 	trip = Trip.objects.get(id=trip_id)
+
+# 	if request.method == 'POST':
+# 		form = TripForm(request.POST, instance=trip)
+# 		if form.is_valid():
+# 			new_Trip = form.save()
+# 			return redirect('trip_details', trip.id)
+# 	else:
+# 		form = TripForm(instance=trip)
+# 	return render(request, 'trip_update.html', {'form': form})
