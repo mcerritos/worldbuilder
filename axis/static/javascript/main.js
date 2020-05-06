d2886f1278444c64c879fd5a64582fd550a1d069
Melody <leclecticat@gmail.com>
2020-05-06 15:32:55 -0700

    adds prepopulated data to post form

--- axis/static/javascript/main.js
+++ axis/static/javascript/main.js
@@ -1,9 +1,9 @@
 console.log("This is Captain Kirk speaking.")
 
-//////// functions to get project id and attach it to posts for updates
+//////// functions to get project id and attach it to project for updates
 let projectId;
-let allUpdateButtons = document.querySelectorAll(".openUpdateModal");
-allUpdateButtons.forEach( (ele) => {
+let allUpdateProjectButtons = document.querySelectorAll(".openUpdateModal");
+allUpdateProjectButtons.forEach( (ele) => {
 	ele.addEventListener('click', setProjectId);
 });
 //update route to use the correct project id 
@@ -17,7 +17,24 @@ function setProjectId (event) {
     formname.value = document.querySelector('span[class*="' + projectId +'"]').textContent;
     formdis.value = document.querySelector('p[class*="' + projectId +'"]').textContent;
 }
-// take the values from the page and put them in the form 
-// get the element and then set the value 
+
+//////// functions to get post id for post update
+let postId;
+let allUpdatePostButtons = document.querySelectorAll(".openUpdatePost");
+allUpdatePostButtons.forEach( (ele) => {
+	ele.addEventListener('click', setPostId);
+});
+// update route on modal
+function setPostId (event) {
+    postId = event.target.getAttribute('data-postid');
+    console.log(postId);
+    postId = postId.replace('p','');
+    document.projectUpdateForm.action = `post/${postId}/update`
+    // these functions get the information from the page and load it into the document
+    let formtitle = document.querySelector('#updateTitle');
+    let formtext = document.querySelector('#updateText');
+    formtitle.value = document.querySelector('h6[class*="' + postId +'"]').textContent;
+    formtext.value = document.querySelector('p[class*="' + postId +'"]').textContent;
+}
 
 
--- axis/urls.py
+++ axis/urls.py
@@ -3,11 +3,14 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('accounts/signup', views.signup, name='signup'),
     #project management
+    path('accounts/signup', views.signup, name='signup'),
     path('accounts/profile', views.profile, name='profile'),
     path('project/<int:project_id>/delete', views.project_delete, name='project_delete'),
     path('accounts/project/<int:project_id>/update', views.project_update, name='project_update'),
+    #posts
+    path('post/<int:post_id>/delete', views.post_delete, name='post_delete'),
+    path('post/<int:post_id>/update', views.post_update, name='post_update'),
     # domains
     path('geography/', views.geography, name='geography'),
     path('culture/', views.culture, name='culture'),
@@ -15,7 +18,4 @@ urlpatterns = [
     path('history/', views.history, name='history'),
     path('religion/', views.religion, name='religion'),
     path('warfare/', views.warfare, name='warfare'),
-    #posts
-    path('post/<int:post_id>/delete', views.post_delete, name='post_delete'),
-    
 ]
