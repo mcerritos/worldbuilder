e9e1c98695fce6211512a81fbe0456a0699533e0
Melody <leclecticat@gmail.com>
2020-05-08 16:20:48 -0700

    fixing anon user error

--- axis/views.py
+++ axis/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-placeholder = Project.objects.get(name="Anonymous")
+placeholder = Project.objects.filter(name="Anonymous")[0]
 
 def home(request):
   if not request.user.is_authenticated:
